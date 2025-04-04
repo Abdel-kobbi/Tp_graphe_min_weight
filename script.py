bad9a5d83167cf0551781e42ce95360018073001
abdel-kobbi <mr.abdelkhaliqkobbi@gmail.com>
2025-04-04 20:29:38 +0000

    implement tree solution logic

--- script.py
+++ script.py
@@ -43,5 +43,34 @@ def read_matrice(path):
 
 matrice = read_matrice(r"matrix.txt")
 
-for line in matrice:
-    print(line)
+# function to find tree solution
+def find_tree_solution(matrix):
+    length_matrice = len(matrix)  # Nombre de sommet
+    node_solution = [randint(0, length_matrice - 1)]  # Liste des sommets inclus dans l’arbre initialiser par sommet au hasard
+    tree_solution = []  # Liste des arêtes
+    weight_total = 0  # Coût total
+
+    while len(node_solution) < length_matrice:
+        minValue = float("inf")
+        index = float("inf")
+        u = float("inf")
+
+        for node in node_solution:
+            for v, weight in enumerate(matrix[node]):
+                if (weight != 0) and (v not in node_solution) and (weight < minValue):
+                    minValue = weight
+                    index = v
+                    u = node
+
+        if index != float("inf"):
+            node_solution.append(index)
+            tree_solution.append((u, index))
+            weight_total += minValue
+    
+    return node_solution, tree_solution, weight_total
+
+solution = find_tree_solution(matrice)
+print("node solution: ",   solution[0],
+      "\nTree solution: ", solution[1],
+        "\nWeight total",  solution[2],
+    )
